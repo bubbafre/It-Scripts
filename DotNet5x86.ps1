654b0521127ef396cb00ea9095125aa7d1d26980
bubbafre <81110173+bubbafre@users.noreply.github.com>
2022-06-15 12:59:28 -0700

    Create DotNet5x86.ps1

---
+++ DotNet5x86.ps1
@@ -0,0 +1,19 @@
+###### Vars#####
+# powershell -command "iex ((New-Object System.Net.WebClient).DownloadString('ps1'))" 
+$URL = "https://download.visualstudio.microsoft.com/download/pr/4a79fcd5-d61b-4606-8496-68071c8099c6/2bf770ca40521e8c4563072592eadd06/dotnet-runtime-5.0.13-win-x86.exe"
+$OutputURL = "C:\temp\dotnet-runtime-5.0.13-win-x86.exe"
+
+#creating temp folder
+mkdir C:\temp\ -ErrorAction SilentlyContinue
+
+$software =  "Microsoft .NET Runtime - 5"
+$installed = (Get-ItemProperty HKLM:\SOFTWARE\Classes\Installer\Products\* | Where { $_.ProductName -match $software }) -ne $null
+
+If(-Not $installed) {
+    Write-Host "'$software' NOT is installed."
+    Write-host "Starting Dotnet-runtime x86 installation"
+##Starting Install
+Write-host Starting $software installation
+Start-Process -NoNewWindow -FilePath "C:\temp\dotnet-runtime-5.0.13-win-x86.exe" -ArgumentList "/install /quiet /norestart"
+
+} else { Write-host $software already installed }
